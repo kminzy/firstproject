a7126df30497c769e19c54f6a28c67edb057b75e
kminzy <kminzy0704@gmail.com>
2021-06-14 20:23:31 +0900

    1

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b^y#&6@luq_yvvlf2#+yy%jt&=v%0nu2h1mh6lrd9h*#v5s+(m'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-b^y#&6@luq_yvvlf2#+yy%jt&=v%0nu2h1mh6lrd9h*#v5s+(m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
