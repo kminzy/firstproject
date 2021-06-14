521e1eedfdcc43b435534cea6481b7752fdb085f
kminzy <kminzy0704@gmail.com>
2021-06-14 20:49:12 +0900

    setting

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2'
-        'NAME' : os.environ.get('DB_NAME'),
-        'USER' : os.environ.get('DB_USER'),
-        'PASSWORD' : os.environ.get('DB_PASSWORD'),
-        'HOST' : os.environ.get('DB_HOST'),
-        'PORT' : '',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '',
     }
 }
 
